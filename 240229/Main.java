aa84b9361857e93151426cc48c64ceb9f112d046
Bruno Ribeiro Batista <bruno@engsupport.eti.br>
2024-02-29 21:37:52 -0300

    Initial commit

---
+++ 240229/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Lexer lex = new Lexer();
+		Parser parse = new Parser(lex);
+		parse.program();
+		System.out.write('\n');
+	}
+}
